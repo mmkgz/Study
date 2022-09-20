d523036c3c09a5f7c49aa182062073cd0af967bf
mmkgz <107329765+mmkgz@users.noreply.github.com>
2022-09-19 22:26:50 -0400

    Add files via upload

---
+++ 6thGradePortal.js
@@ -0,0 +1,20 @@
+var socialStudies=[['Unit 1','Elements of Culture',
+['Your personal identity, made up of shared behaviors and values that are broken into elements.',
+'The place where elements of culture started.',
+'The spreading of Culture from one Place to another.',
+'A celebration from the past that people still practice today. Passed down through family generations.',
+'Human communication that is written, spoken, or signed.',
+'An organized system of beliefs, ceremonies and rules used to worship a god or a group of gods.',
+'A type of religion that believes in MANY gods.',
+'A type of religion that believes in ONE god.',
+'Your family\'s background based on where your ancestors are from that influences your culture and traditions.',
+'The understanding that each person is unique and recognizing everyone\'s differences.'
+],
+['Culture','Cultural Hearth','Cultural Diffusion','Tradition','Language','Religion','Polytheism','Monotheism','Heritage','Diversity']],
+
+['Early Humans','Unit 2',['An early ancestor of humans that developed in Africa & moved around the world','Early humans who hunt animals and gather wild plants, seeds, fruits, and nuts to survive.','Constantly moving from place to place to provide for you basic needs','Known as the old stone age. Early humans used stone tools and moved to survive.','Known as the new stone age. Early humans stopped moving and started farming','A time of climate change when huge ice glaciers covered the Earth\'s surface allowing the early humans to travel over the frozen water to new land.','A scientist who studies prehistoric people by analyzing artifacts, inscriptions, and monuments from the past','A form of communication used during a time of no spoken language. Used to tell stories, pass along information, or track animals.','A strip of land that connects two continents together that aren\'t normally connected'],['Hominid','Hunter-Gatherers','Nomad Lifestyle','Paleolithic Era','Neolithic Era','Ice Age','Archaeologist','Cave Paintings','Land Bridge']]
+
+];
+var math=[];
+var science=[];
+var ela=[];
